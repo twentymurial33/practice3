a25e1d23c0b6d19923db590fbaaff26ee5c35b70
Murial Anindo <murial.anindo@lonett.com>
2021-09-17 09:54:54 -0400

    changes

---
+++ main.js
@@ -0,0 +1,30 @@
+// Put DOM elements into variables
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
+//listen for form submit
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "Please enter all fields";
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement("li");
+
+    li.appendChild(
+      document.createTextNode(`${nameInput.value}:${emailInput.value}`)
+    );
+
+    userList.appendChild("li");
+
+    nameInput.value = "";
+    emailInput.value = "";
+  }
+}
